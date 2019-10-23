f0c0ff704b3c97c1767b21ed4bdf19f89234e2c9
NamelessOgya <hakuginnnohana@gmail.com>
2019-10-23 19:59:43 +0900

    add filter to PlayerResult

--- attendance/admin.py
+++ attendance/admin.py
@@ -13,7 +13,8 @@ class RegisteredAdmin(admin.ModelAdmin):
 class TableAdmin(admin.ModelAdmin):
     list_filter = ["date", "season"]
 
-
+class PlayerResultAdmin(admin.ModelAdmin):
+    list_filter = ["date"]
 
 
 
@@ -23,7 +24,7 @@ admin.site.register(Match)
 admin.site.register(Registered, RegisteredAdmin)
 admin.site.register(Table, TableAdmin)
 admin.site.register(Reported, ReportedAdmin)
-admin.site.register(PlayerResult)
+admin.site.register(PlayerResult, PlayerResultAdmin)
 admin.site.register(TeamResult)
 admin.site.register(ClassWinRate)
 admin.site.register(Blog)
