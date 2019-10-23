6277b983b990d6e451e9b975d2a42916c11b389d
NamelessOgya <hakuginnnohana@gmail.com>
2019-10-23 19:56:23 +0900

    add filter in table

--- attendance/admin.py
+++ attendance/admin.py
@@ -7,13 +7,21 @@ class PlayerAdmin(admin.ModelAdmin):
 class ReportedAdmin(admin.ModelAdmin):
     list_filter = ["date", "team"]
 
+class RegisteredAdmin(admin.ModelAdmin):
+    list_filter = ["date", "team"]
+
+class TableAdmin(admin.ModelAdmin):
+    list_filter = ["date", "season"]
+
+
+
 
 
 admin.site.register(Team)
 admin.site.register(Player, PlayerAdmin)
 admin.site.register(Match)
-admin.site.register(Registered)
-admin.site.register(Table)
+admin.site.register(Registered, RegisteredAdmin)
+admin.site.register(Table, TableAdmin)
 admin.site.register(Reported, ReportedAdmin)
 admin.site.register(PlayerResult)
 admin.site.register(TeamResult)
