4fe3be5d07d48b9f23d9651b4ed3525ea24a6bc1
NamelessOgya <hakuginnnohana@gmail.com>
2019-10-23 17:43:23 +0900

    add filter in reported

--- attendance/admin.py
+++ attendance/admin.py
@@ -4,7 +4,8 @@ from .models import Team, Player, Match, Registered, Table, Reported, PlayerResu
 # Register your models here.
 class PlayerAdmin(admin.ModelAdmin):
     list_filter = ["team", "season"]
-
+class ReportedAdmin(admin.ModelAdmin):
+    list_filter = ["date", "team"]
 
 
 
@@ -13,7 +14,7 @@ admin.site.register(Player, PlayerAdmin)
 admin.site.register(Match)
 admin.site.register(Registered)
 admin.site.register(Table)
-admin.site.register(Reported)
+admin.site.register(Reported, ReportedAdmin)
 admin.site.register(PlayerResult)
 admin.site.register(TeamResult)
 admin.site.register(ClassWinRate)
